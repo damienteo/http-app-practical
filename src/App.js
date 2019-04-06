619f99dda2fb196dd4b51028c974ea6122569d11
damienteo <ksteo89@gmail.com>
2019-04-07 01:53:25 +0800

    endpoint is in config json

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import http from "./services/httpService";
+import config from "./config.json";
 import "./App.css";
-
-const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
 
 class App extends Component {
   state = {
@@ -15,21 +14,21 @@ class App extends Component {
   async componentDidMount() {
     // const promise = http.get("https://jsonplaceholder.typicode.com/posts");
     // const response = await promise;
-    const { data: posts } = await http.get(apiEndpoint);
+    const { data: posts } = await http.get(config.apiEndpoint);
     this.setState({ posts });
   }
 
   handleAdd = async () => {
     const obj = { title: "a", body: "b" };
-    const { data: post } = await http.post(apiEndpoint, obj);
+    const { data: post } = await http.post(config.apiEndpoint, obj);
     const posts = [post, ...this.state.posts];
     this.setState({ posts });
   };
 
   handleUpdate = async post => {
     post.title = "UPDATED";
-    await http.put(apiEndpoint + "/" + post.id, post);
-    // http.patch(apiEndpoint + "/" + post.id, { title: post.title });
+    await http.put(config.apiEndpoint + "/" + post.id, post);
+    // http.patch(config.apiEndpoint + "/" + post.id, { title: post.title });
     const posts = [...this.state.posts];
     const index = posts.indexOf(post);
     posts[index] = { ...post };
@@ -43,7 +42,7 @@ class App extends Component {
     this.setState({ posts });
 
     try {
-      await http.delete(apiEndpoint + "/" + post.id);
+      await http.delete(config.apiEndpoint + "/" + post.id);
       // throw new Error("");
     } catch (ex) {
       // expected (404: not found, 400: bad request) - client errors
